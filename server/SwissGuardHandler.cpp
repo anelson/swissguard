2419d7c85e6a6d1de82753a4304551c072643e37
Adam Nelson <anelson@apocryph.org>
2013-11-29 12:13:16 -0500

    Implement initial working version; still not sure all of the variants of the position system are correct

--- server/SwissGuardHandler.cpp
+++ server/SwissGuardHandler.cpp
@@ -12,6 +12,9 @@
 
 #include "swephexp.h"
 
+#define SECONDS_PER_DAY 86400.0
+
+
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
@@ -29,18 +32,18 @@ class SwissGuardHandler : virtual public SwissGuardServiceIf
   virtual void setJplFile(const std::string& jplFilePath) {
   	//Of course could use boost for this but running boost under OS X right now is a royal PITA
   	int beginIdx = jplFilePath.rfind('/');
-	auto fileName = jplFilePath.substr(beginIdx + 1);
-	auto directory = jplFilePath.substr(0, beginIdx-1);
+  	auto fileName = jplFilePath.substr(beginIdx + 1);
+  	auto directory = jplFilePath.substr(0, beginIdx);
 
-	swe_set_ephe_path(const_cast<char*>(directory.c_str()));
-	swe_set_jpl_file(const_cast<char*>(fileName.c_str()));
+  	swe_set_ephe_path(const_cast<char*>(directory.c_str()));
+  	swe_set_jpl_file(const_cast<char*>(fileName.c_str()));
 
-	cout << "swisseph path set to " << directory << endl;
-	cout << "swisseph JPL file set to " << fileName << endl;
+  	cout << "swisseph path set to " << directory << endl;
+  	cout << "swisseph JPL file set to " << fileName << endl;
   }
 
   virtual void computeBodyStates(astrothrift::BodyStates& _return, const astrothrift::Bodies& bodies, const astrothrift::PositionSystem position, const astrothrift::CoordinateSystem coords, const astrothrift::JulianDate& date) {
-  	int flags = 0;
+  	int flags = SEFLG_JPLEPH | SEFLG_SPEED;
 
   	//Build up the flags based on the parameters specified
   	switch (position) {
@@ -53,7 +56,7 @@ class SwissGuardHandler : virtual public SwissGuardServiceIf
 			break;
 
 		case astrothrift::PositionSystem::Barycentric:
-			flags |= SEFLG_BARYCTR;
+			flags |= SEFLG_BARYCTR & SEFLG_ICRS & SEFLG_J2000 & SEFLG_NOABERR & SEFLG_NOGDEFL & SEFLG_NONUT;
 			break;
 
 		case astrothrift::PositionSystem::Astrometric:
@@ -108,19 +111,27 @@ class SwissGuardHandler : virtual public SwissGuardServiceIf
   		if (result == ERR) {
   			cerr << "Got error getting state for body " << bodyNumber << " with flags " << flags << ": " << errorMessage << endl;
 
-  			auto e = SwissEphemerisError();
+  			auto e = astrothrift::AstroThriftError();
   			e.message = errorMessage;
 
   			throw e;
   		}
 
   		astrothrift::State bodyState;
+
   		bodyState.pos.x = state[0];
   		bodyState.pos.y = state[1];
   		bodyState.pos.z = state[2];
   		bodyState.vel.rx = state[3];
   		bodyState.vel.ry = state[4];
   		bodyState.vel.rz = state[5];
+
+      //When dealing with cartesian coordinates, speed is returned in AU/day.  It should be AU/sec per astrothrift
+      if (flags & SEFLG_XYZ) {
+        bodyState.vel.rx /= SECONDS_PER_DAY;
+        bodyState.vel.ry /= SECONDS_PER_DAY;
+        bodyState.vel.rz /= SECONDS_PER_DAY;
+      }
 
   		_return[bodyNumber] = bodyState;
   	}
@@ -135,7 +146,7 @@ int main (int argc, char **argv)
   shared_ptr<swissguard::SwissGuardHandler> handler(new swissguard::SwissGuardHandler());
   shared_ptr<TProcessor> processor(new swissguard::SwissGuardServiceProcessor(handler));
   shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-  shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
+  shared_ptr<TTransportFactory> transportFactory(new TFramedTransportFactory());
   shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
   shared_ptr<ThreadManager> threadManager = ThreadManager::newSimpleThreadManager (1);
   shared_ptr<PosixThreadFactory> threadFactory    = shared_ptr<PosixThreadFactory> (new PosixThreadFactory ());
