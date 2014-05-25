a12b179836dc678f4b4df929bb71057a16464514
Adam Nelson <anelson@apocryph.org>
2014-05-25 13:38:25 -0400

    Implement a new approach with an API closely resembling that of native swisseph

--- server/SwissGuardHandler.cpp
+++ server/SwissGuardHandler.cpp
@@ -10,6 +10,8 @@
 #include <thrift/concurrency/PosixThreadFactory.h>
 #include <thrift/server/TThreadedServer.h>
 
+#include <boost/format.hpp>
+
 #include "swephexp.h"
 
 #define SECONDS_PER_DAY 86400.0
@@ -24,11 +26,13 @@ using boost::shared_ptr;
 using std::cout;
 using std::cerr;
 using std::endl;
+using boost::format;
 
 namespace swissguard {
 
 class SwissGuardHandler : virtual public SwissGuardServiceIf
 {
+  /*
   virtual void setJplFile(const std::string& jplFilePath) {
   	//Of course could use boost for this but running boost under OS X right now is a royal PITA
   	int beginIdx = jplFilePath.rfind('/');
@@ -42,67 +46,67 @@ class SwissGuardHandler : virtual public SwissGuardServiceIf
   	cout << "swisseph JPL file set to " << fileName << endl;
   }
 
-  virtual void computeBodyStates(astrothrift::BodyStates& _return, const astrothrift::Bodies& bodies, const astrothrift::PositionSystem position, const astrothrift::CoordinateSystem coords, const astrothrift::JulianDate& date) {
+  virtual void computeBodyStates(BodyStates& _return, const Bodies& bodies, const PositionSystem position, const CoordinateSystem coords, const JulianDate& date) {
   	int flags = SEFLG_JPLEPH | SEFLG_SPEED;
 
   	//Build up the flags based on the parameters specified
   	switch (position) {
-  		case astrothrift::PositionSystem::TrueGeocentric:
+  		case PositionSystem::TrueGeocentric:
   			flags |= SEFLG_TRUEPOS;
   			break;
 
-		case astrothrift::PositionSystem::Heliocentric:
+		case PositionSystem::Heliocentric:
 			flags |= SEFLG_HELCTR;
 			break;
 
-		case astrothrift::PositionSystem::Barycentric:
+		case PositionSystem::Barycentric:
 			flags |= SEFLG_BARYCTR & SEFLG_ICRS & SEFLG_J2000 & SEFLG_NOABERR & SEFLG_NOGDEFL & SEFLG_NONUT;
 			break;
 
-		case astrothrift::PositionSystem::Astrometric:
+		case PositionSystem::Astrometric:
 			flags |= SEFLG_NOABERR & SEFLG_NOGDEFL;
 			break;
 
-		case astrothrift::PositionSystem::AstrometricJ2000:
+		case PositionSystem::AstrometricJ2000:
 			flags |= SEFLG_NOABERR & SEFLG_NOGDEFL & SEFLG_J2000 & SEFLG_NONUT;
 			break;
 
-		case astrothrift::PositionSystem::MeanDate:
+		case PositionSystem::MeanDate:
 			flags |= SEFLG_NONUT;
 			break;
 
-		case astrothrift::PositionSystem::MeanJ2000:
+		case PositionSystem::MeanJ2000:
 			flags |= SEFLG_J2000 & SEFLG_NONUT;
 			break;
 
-		case astrothrift::PositionSystem::TrueDate:
+		case PositionSystem::TrueDate:
 			//This is the default; leave flags as they are
 			break;
 
-		case astrothrift::PositionSystem::TrueJ2000:
+		case PositionSystem::TrueJ2000:
 			flags |= SEFLG_J2000;
 			break;
   	}
 
   	switch (coords) {
-  		case astrothrift::EclipticSpherical:
+  		case EclipticSpherical:
   			//This is the default; leave flags alone
   			break;
 
-		case astrothrift::EclipticRectangular:
+		case EclipticRectangular:
 			flags |= SEFLG_XYZ;
 			break;
 
-		case astrothrift::EquatorialSpherical:
+		case EquatorialSpherical:
 			flags |= SEFLG_EQUATORIAL;
 			break;
 
-		case astrothrift::EquatorialRectangular:
+		case EquatorialRectangular:
 			flags |= SEFLG_EQUATORIAL | SEFLG_XYZ;
 			break;
   	}
 
-  	for (astrothrift::Bodies::const_iterator iter = bodies.begin(); iter != bodies.end(); ++iter) {
+  	for (Bodies::const_iterator iter = bodies.begin(); iter != bodies.end(); ++iter) {
   		char errorMessage[256];
   		double state[6] = {0};
   		auto bodyNumber = *iter;
@@ -111,13 +115,13 @@ class SwissGuardHandler : virtual public SwissGuardServiceIf
   		if (result == ERR) {
   			cerr << "Got error getting state for body " << bodyNumber << " with flags " << flags << ": " << errorMessage << endl;
 
-  			auto e = astrothrift::AstroThriftError();
+  			auto e = AstroThriftError();
   			e.message = errorMessage;
 
   			throw e;
   		}
 
-  		astrothrift::State bodyState;
+  		State bodyState;
 
   		bodyState.pos.x = state[0];
   		bodyState.pos.y = state[1];
@@ -135,6 +139,175 @@ class SwissGuardHandler : virtual public SwissGuardServiceIf
 
   		_return[bodyNumber] = bodyState;
   	}
+  }
+  */
+  virtual void getVersion(std::string& _return) {
+    char version[255] = {0};
+    swe_version(version);
+
+    _return = version;
+  }
+
+  virtual void calculateBodyState(State& _return, const double jdEt, const int32_t bodyId, const Flags flags) {
+    double xx[6] = {0};
+    char message[255] = {0};
+
+    int ret = swe_calc(jdEt, bodyId, flags, xx, message);
+
+    checkReturnFlag(flags, ret, "swe_calc", message);
+
+    stateFromVector(xx, _return);
+  }
+
+  virtual void calculateBodiesState(States& _return, const double jdEt, const BodyIds& bodyIds, const Flags flags) {
+    for (auto &id : bodyIds) {
+      State state;
+      calculateBodyState(state, jdEt, id, flags);
+      _return.push_back(state);
+    }
+  }
+
+  virtual void calculateBodyStateUt(State& _return, const double jdUt, const int32_t bodyId, const Flags flags) {
+    double xx[6] = {0};
+    char message[255] = {0};
+
+    int ret = swe_calc_ut(jdUt, bodyId, flags, xx, message);
+
+    checkReturnFlag(flags, ret, "swe_calc_ut", message);
+
+    stateFromVector(xx, _return);
+  }
+
+  virtual void calculateBodiesStateUt(States& _return, const double jdUt, const BodyIds& bodyIds, const Flags flags) {
+    for (auto &id : bodyIds) {
+      State state;
+      calculateBodyStateUt(state, jdUt, id, flags);
+      _return.push_back(state);
+    }
+  }
+
+  virtual void calculateFixedStarState(State& _return, const double jdEt, const std::string& starName, const Flags flags) {
+    double xx[6] = {0};
+    char message[255] = {0};
+
+    int ret = swe_fixstar(const_cast<char*>(starName.c_str()), jdEt, flags, xx, message);
+
+    checkReturnFlag(flags, ret, "swe_fixstar", message);
+
+    stateFromVector(xx, _return);
+  }
+
+  virtual void calculateFixedStarsState(States& _return, const double jdEt, const StarNames& starNames, const Flags flags) {
+    for (auto &name : starNames) {
+      State state;
+      calculateFixedStarState(state, jdEt, name, flags);
+      _return.push_back(state);
+    }
+  }
+
+  virtual void calculateFixedStarStateUt(State& _return, const double jdUt, const std::string& starName, const Flags flags) {
+    double xx[6] = {0};
+    char message[255] = {0};
+
+    int ret = swe_fixstar(const_cast<char*>(starName.c_str()), jdUt, flags, xx, message);
+
+    checkReturnFlag(flags, ret, "swe_fixstar_ut", message);
+
+    stateFromVector(xx, _return);
+  }
+
+  virtual void calculateFixedStarsStateUt(States& _return, const double jdUt, const StarNames& starNames, const Flags flags) {
+    for (auto &name : starNames) {
+      State state;
+      calculateFixedStarStateUt(state, jdUt, name, flags);
+      _return.push_back(state);
+    }
+  }
+
+  virtual void computeHouses(HouseCusps& _return, const double jdUt, const double geographicLatitude, const double geographicLongitude, const std::string& houseSystem) {
+    double cusps[13] = {0};
+    double ascmc[10] = {0};
+
+    int ret = swe_houses(jdUt, geographicLatitude, geographicLongitude, static_cast<int>(houseSystem[0]), cusps, ascmc);
+
+    checkReturnFlag(ret, "swe_houses", "<no message>");
+
+    houseCuspsFromVectors(cusps, ascmc, _return);
+  }
+
+  virtual void computeHousesArmc(HouseCusps& _return, const double armc, const double geographicLatitude, const double eclipticObliquityDegrees, const std::string& houseSystem) {
+    double cusps[13] = {0};
+    double ascmc[10] = {0};
+
+    int ret = swe_houses_armc(armc, geographicLatitude, eclipticObliquityDegrees, static_cast<int>(houseSystem[0]), cusps, ascmc);
+
+    checkReturnFlag(ret, "swe_houses_armc", "<no message>");
+
+    houseCuspsFromVectors(cusps, ascmc, _return);
+  }
+
+  virtual void computeHousesEx(HouseCusps& _return, const double jdUt, const Flags flags, const double geographicLatitude, const double geographicLongitude, const std::string& houseSystem) {
+    double cusps[13] = {0};
+    double ascmc[10] = {0};
+
+    int ret = swe_houses_ex(jdUt, flags, geographicLatitude, geographicLongitude, static_cast<int>(houseSystem[0]), cusps, ascmc);
+
+    checkReturnFlag(ret, "swe_houses_ex", "<no message>");
+
+    houseCuspsFromVectors(cusps, ascmc, _return);
+  }
+
+private:
+  void checkReturnFlag(int32_t flags, int32_t returnFlags, const char* methodName, const char* message) {
+    //Check for errors, and throw an exception
+    checkReturnFlag(returnFlags, methodName, message);
+
+    if (returnFlags != flags) {
+      //A soft error; means one of the flag bits couldn't be applied to the calculation for some reason.  For our purposes that's an error
+      auto e = SwissEphemerisError();
+
+      e.message = str(format("%s error: input flags 0x%08x, return flags 0x%08x") % methodName % flags % returnFlags);
+
+      throw e;
+    }
+  }
+
+  void checkReturnFlag(int32_t returnFlags, const char* methodName, const char* message) {
+    //Check for errors, and throw an exception
+    if (returnFlags < 0) {
+      //A straight-up error case:
+      auto e = SwissEphemerisError();
+
+      e.message = str(format("%s error: %s") % methodName % message);
+
+      throw e;
+    } 
+  }
+
+  void stateFromVector(const double* vector, State& state) {
+    state.pos.x = vector[0];
+    state.pos.y = vector[1];
+    state.pos.z = vector[2];
+
+    state.vel.rx = vector[3];
+    state.vel.ry = vector[4];
+    state.vel.rz = vector[5];
+  }
+
+  void houseCuspsFromVectors(const double* cusps, const double* ascmc, HouseCusps& houseCusps) {
+    //Note for some stupid reason, swisseph uses 1-based indexing for house cusps, with element 0 always set to 0.0
+    for (auto i = 1; i < 13; i++) {
+      houseCusps.cusps.push_back(cusps[i]);
+    }
+
+    houseCusps.ascendant = ascmc[SE_ASC];
+    houseCusps.mc = ascmc[SE_MC];
+    houseCusps.armc = ascmc[SE_ARMC];
+    houseCusps.vertex = ascmc[SE_VERTEX];
+    houseCusps.equatorialAscendant = ascmc[SE_EQUASC];
+    houseCusps.coAscendantKoch = ascmc[SE_COASC1];
+    houseCusps.coAscendantMunkasey = ascmc[SE_COASC2];
+    houseCusps.polarAscendant = ascmc[SE_POLASC];
   }
 };
 
--- swissguard.thrift
+++ swissguard.thrift
@@ -1,16 +1,144 @@
-include "astro.thrift"
-
-/** Defines the service and types exposed by swissguard to wrap the swisseph library */
 namespace cpp swissguard
 namespace java swissguard
+
+enum BodyId {
+	SUN = 0,
+	MOON = 1,
+	MERCURY = 2,
+	VENUS = 3,
+	MARS = 4,
+	JUPITER = 5,
+	SATURN = 6,
+	URANUS = 7,
+	NEPTUNE = 8,
+	PLUTO = 9,
+	MEAN_NODE = 10,
+	TRUE_NODE = 11,
+	MEAN_APOG = 12,
+	OSCU_APOG = 13,
+	EARTH = 14,
+	CHIRON = 15,
+	PHOLUS = 16,
+	CERES = 17,
+	PALLAS = 18,
+	JUNO = 19,
+	VESTA = 20,
+	INTP_APOG = 21,
+	INTP_PERG = 22,
+	NPLANETS = 23,
+	AST_OFFSET = 10000,
+	VARUNA = 30000 /* AST_OFFSET + 20000 */,
+	FICT_OFFSET = 40,
+	FICT_OFFSET_1 = 39,
+	FICT_MAX = 999,
+	NFICT_ELEM = 15,
+	COMET_OFFSET = 1000,
+	NALL_NAT_POINTS = 38 /*(NPLANETS + NFICT_ELEM) */,
+	CUPIDO = 40,
+	HADES = 41,
+	ZEUS = 42,
+	KRONOS = 43,
+	APOLLON = 44,
+	ADMETOS = 45,
+	VULKANUS = 46,
+	POSEIDON = 47,
+	ISIS = 48,
+	NIBIRU = 49,
+	HARRINGTON = 50,
+	NEPTUNE_LEVERRIER = 51,
+	NEPTUNE_ADAMS = 52,
+	PLUTO_LOWELL = 53,
+	PLUTO_PICKERING = 54,
+	VULCAN = 55,
+	WHITE_MOON = 56,
+	PROSERPINA = 57,
+	WALDEMATH = 58
+}
+
+enum Flags {
+	JPLEPH = 1,
+	SWIEPH = 2,
+	MOSEPH = 4,
+	HELCTR = 8,
+	TRUEPOS = 16,
+	J2000 = 32,
+	NONUT = 64,
+	SPEED3 = 128,
+	SPEED = 256,
+	NOGDEFL = 512,
+	NOABERR = 1024,
+	EQUATORIAL = 2048 /*(2*1024)*/,
+	XYZ = 4096 /*(4*1024) */,
+	RADIANS = 8192 /*(8*1024) */,
+	BARYCTR = 16384 /*(16*1024) */,
+	TOPOCTR = 32768 /*(32*1024) */,
+	SIDEREAL = 65536 /*(64*1024)*/,
+	ICRS = 131072 /*(128*1024)*/
+}
+
+/** Position of a body, either long, lat, distance (degrees) or x, y, z (AU) depending upon coordinate system */
+struct Position {
+	1: double x,
+	2: double y,
+	3: double z
+}
+
+/** Velocity of a body, either deg long/day, deg lat/day, distance/day, or rx, ry, rz (AU/sec) depending upon coordinate system */
+struct Velocity {
+	1: double rx,
+	2: double ry,
+	3: double rz
+}
+
+struct State {
+	1: Position pos,
+	2: Velocity vel
+}
+
+struct HouseCusps {
+	1: list<double> cusps,
+	2: double ascendant,
+	3: double mc,
+	4: double armc,
+	5: double vertex,
+	6: double equatorialAscendant,
+	7: double coAscendantKoch,
+	8: double coAscendantMunkasey,
+	9: double polarAscendant
+}
 
 exception SwissEphemerisError {
 	1: string message
 }
 
-service SwissGuardService extends astro.AstroThriftService {
-	/** Sets the path of the JPL ephemeris file to use.  This must be called first and refer to a JPL ephemeris file that swisseph
-	knows how to read, or attempts to get body state wil fail */
-	void setJplFile(1: string jplFilePath) throws (1: SwissEphemerisError see)
-}
+typedef list<i32> BodyIds
 
+typedef list<string> StarNames
+
+typedef list<State> States
+
+service SwissGuardService {
+	string getVersion() throws (1: SwissEphemerisError see)
+
+	State calculateBodyState(1: double jdEt, 2: i32 bodyId, 3: Flags flags) throws (1: SwissEphemerisError see)
+
+	States calculateBodiesState(1: double jdEt, 2: BodyIds bodyIds, 3: Flags flags) throws (1: SwissEphemerisError see)
+
+	State calculateBodyStateUt(1: double jdUt, 2: i32 bodyId, 3: Flags flags) throws (1: SwissEphemerisError see)
+
+	States calculateBodiesStateUt(1: double jdUt, 2: BodyIds bodyIds, 3: Flags flags) throws (1: SwissEphemerisError see)
+
+	State calculateFixedStarState(1: double jdEt, 2: string starName, 3: Flags flags) throws (1: SwissEphemerisError see)
+
+	States calculateFixedStarsState(1: double jdEt, 2: StarNames starNames, 3: Flags flags) throws (1: SwissEphemerisError see)
+
+	State calculateFixedStarStateUt(1: double jdUt, 2: string starName, 3: Flags flags) throws (1: SwissEphemerisError see)
+
+	States calculateFixedStarsStateUt(1: double jdUt, 2: StarNames starNames, 3: Flags flags) throws (1: SwissEphemerisError see)
+
+	HouseCusps computeHouses(1: double jdUt, 2: double geographicLatitude, 3: double geographicLongitude, 4: string houseSystem) throws (1: SwissEphemerisError see)
+
+	HouseCusps computeHousesArmc(1: double armc, 2: double geographicLatitude, 3: double eclipticObliquityDegrees, 4: string houseSystem) throws (1: SwissEphemerisError see)
+
+	HouseCusps computeHousesEx(1: double jdUt, 2: Flags flags, 3: double geographicLatitude, 4: double geographicLongitude, 5: string houseSystem) throws (1: SwissEphemerisError see)
+}
